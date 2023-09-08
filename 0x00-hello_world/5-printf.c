93a73a7f305d4830622110a9b6a156314c5cf06d
Osunba Adeola <osunbaadeola56@gmail.com>
2023-09-07 19:51:20 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main	-Entry	Point
+ * main	- Entry point
  *
- * Return:Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	prinf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
