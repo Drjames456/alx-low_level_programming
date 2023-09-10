df2fda0a1f0a9de1f51887176c8e88be799f32a4
Osunba Adeola <osunbaadeola56@gmail.com>
2023-09-10 15:40:06 -0700

    C lang

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+/**
+ * main- this is the main function for numbers
+ * possible two digits numbers
+ * Return: Always 0 (success)
+ */
+
+
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = a; b <= '9'; b++)
+		{
+			if (a != b)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
