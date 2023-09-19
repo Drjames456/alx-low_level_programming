15066c6db80c74f084ada6d00ce34d4f4e0d82e2
Osunba Adeola <osunbaadeola56@gmail.com>
2023-09-19 08:23:46 -0700

    c lang

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * reset_to_98- A function that updates the value it points to
+ * @n: Interger to change
+ * Return: Nothing.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
