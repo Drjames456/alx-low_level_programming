5f11b045d925dad856a32de35f0321b4b12fcfac
Osunba Adeola <osunbaadeola56@gmail.com>
2023-09-21 07:38:04 -0700

    Love c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ * _strcat - a function that concatenates two strings
+ * @dest: the first string
+ * @src: the second string
+ * Return: returns dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+
+	printf("%c", *dest + *src);
+
+	return (dest);
+}
+
+
