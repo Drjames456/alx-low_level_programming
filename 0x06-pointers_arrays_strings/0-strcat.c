57f511c5aa5e9aa71e14cf3fcd3d9dcb6e08a948
Osunba Adeola <osunbaadeola56@gmail.com>
2023-09-21 14:03:50 -0700

    c lang

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 
 /**
@@ -10,10 +10,18 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int destlen = 0;
+	int srclen = 0;
+	int i;
 
-
-	printf("%c", *dest + *src);
-
+	for (i = 0; dest[i] != '\0'; i++)
+		destlen++;
+	for (i = 0; src[i] != '\0'; i++)
+		srclen++;
+	for (i = 0; i <= srclen; i++)
+	{
+		dest[destlen + i] = src[i];
+	}
 	return (dest);
 }
 
