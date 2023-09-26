9fc6aa62de2d56120eb7a3afbd26c1027592892b
Osunba Adeola <osunbaadeola56@gmail.com>
2023-09-26 15:42:40 -0700

    c lang

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * set_string - its set a value of pointer to char
+ * @s: input1
+ * @to: input2
+ * Return : Always 0
+ */
+
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
