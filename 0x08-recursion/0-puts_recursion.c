0bb4dfd7f1e5ff1c86731153237879d2b3d9694e
Osunba Adeola <osunbaadeola56@gmail.com>
2023-09-28 07:28:18 -0700

    c lang

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * _puts_recursion - a function that print a string.
+ * @s: input
+ * Return - always 0
+ */
+
+
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
