e541aba1393b0b9e281dbadcb8788120692a6f5c
Osunba Adeola <osunbaadeola56@gmail.com>
2023-10-01 17:20:08 -0700

    c lang

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * is_divisible - its calculate a prin nubmer
+ * @n : enter value
+ * @divisor: to divide n
+ * Reutrn:is_diviible
+ */
+
+
+int is_divisible(int n, int divisor)
+{
+	if (divisor == 1)
+	{
+		return (0);
+	}
+
+	if (divisor == 2)
+	{
+		return ((n % 2 == 0) ? 1 : 0);
+	}
+	if (n % divisor == 0)
+	{
+		return (1);
+	}
+
+	return (is_divisible(n, divisor - 1));
+}
+
+/**
+ * is_prime_number - its returns 1 if the input is integer
+ * @n: value
+ * Return: return 1 or 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (n == 2 || n == 3)
+	{
+		return (1);
+	}
+	return (!is_divisible(n, n - 1));
+}
