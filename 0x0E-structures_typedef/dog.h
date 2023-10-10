79a025f9bb1d12aa0c483f8b21169b17f161e952
Osunba Adeola <osunbaadeola56@gmail.com>
2023-10-10 10:33:32 -0700

    c lang

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - a structured data for a dog
+ * @name: a pointer to a dog's name
+ * @age: dog's age
+ * @owner: a pointer to dog's owner
+ *
+ *
+ *
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
