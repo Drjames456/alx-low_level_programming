1b879665452003a84f105affe65b77d8b0f9685f
Osunba Adeola <osunbaadeola56@gmail.com>
2023-10-20 11:40:14 -0700

    c lang

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * list_len -  a function that returns the number of elements
+ * @h: a pointer
+ * Return: number of elements
+ */
+
+
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
