97f04b075e3a482a517d70fea1186bbfe49eee49
Osunba Adeola <osunbaadeola56@gmail.com>
2023-10-24 02:27:54 -0700

    singly-linked in C

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+
+
+/**
+ * listint_len - a function that returns the number of elements in a linked.
+ * @h: a ptr to the head listint
+ * Return: number of nodes
+ */
+
+
+size_t listint_len(const listint_t *h)
+{
+	size_t  nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
