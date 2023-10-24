76ca2dc8cf37ecc5798065909eab9c226b0720d5
Osunba Adeola <osunbaadeola56@gmail.com>
2023-10-24 10:08:06 -0700

    singly-linked in C

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * free_listint2-  a function that frees a listint_t list.
+ * @head: a ptr to the head of listint_t
+ * Return: void
+ */
+
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+
+
+
+	while (head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+}
